beabe981e154e635b7e6031943732449753b3dc6
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-01 08:59:11 +0900

    Initial Commit

---
+++ AnimalQuiz/AnimalQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalQuizApp.swift
+//  AnimalQuiz
+//
+//  Created by Nobuki Mitsuoka on 2025/03/01.
+//
+
+import SwiftUI
+
+@main
+struct AnimalQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalQuizTests/AnimalQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalQuizTests.swift
+//  AnimalQuizTests
+//
+//  Created by Nobuki Mitsuoka on 2025/03/01.
+//
+
+import Testing
+@testable import AnimalQuiz
+
+struct AnimalQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
