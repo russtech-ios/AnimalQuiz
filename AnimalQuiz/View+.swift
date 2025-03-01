ff5ae707353213c41356845cf4dc41a41904401f
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-01 22:42:19 +0900

    backgroundImageを定義

--- AnimalQuiz/View+.swift
+++ AnimalQuiz/View+.swift
@@ -12,4 +12,16 @@ extension View {
     func stroke(color: Color, width: CGFloat = 1) -> some View {
         modifier(StrokeBackground(strokeSize: width, strokeColor: color))
     }
+    
+    @ViewBuilder
+    func backgroundImage(_ image: Image = Image(.background)) -> some View {
+        self
+            .background(
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .padding(-3)
+            )
+    }
 }
