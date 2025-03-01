40ea019dff3e075691c9a2e5a1ecae7a4e810bef
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-01 21:49:59 +0900

    ScoreView.swiftを作成

---
+++ AnimalQuiz/ScoreView.swift
@@ -0,0 +1,22 @@
+//
+//  ScoreView.swift
+//  AnimalQuiz
+//
+//  Created by Nobuki Mitsuoka on 2025/03/01.
+//
+
+import SwiftUI
+
+struct ScoreView: View {
+    var body: some View {
+        VStack {
+            Text("5問中3問正解！")
+            Button("トップへ") {
+            }
+        }
+    }
+}
+
+#Preview {
+    ScoreView()
+}
