9dab6580c4624f5755001baca0ba288357522cf7
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-01 22:03:13 +0900

    QuizViewからScoreViewへの画面遷移を実装

--- AnimalQuiz/QuizView.swift
+++ AnimalQuiz/QuizView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct QuizView: View {
+    @State var isShowingScoreView = false
+    
     var body: some View {
         VStack {
             Text("問題番号: 1/5")
             Text("次のうち、世界で最も早く走る動物はどれですか？")
             Button("ライオン") {
+                isShowingScoreView = true
+            }
+            .fullScreenCover(isPresented: $isShowingScoreView) {
+                ScoreView()
             }
             Button("ウサイン・ボルト") {
             }
