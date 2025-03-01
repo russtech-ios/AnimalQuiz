532414d3bcbe844f2fab00e7ccbda8c8bdd2ae92
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-01 22:40:47 +0900

    スタート画面のUIを変更

--- AnimalQuiz/StartView.swift
+++ AnimalQuiz/StartView.swift
@@ -12,15 +12,26 @@ struct StartView: View {
     
     var body: some View {
         VStack {
-            Text("動物クイズ！")
-            Button("スタート") {
+            Spacer()
+            Text("どうぶつ\nクイズ！")
+                .font(.system(size: 70).bold())
+                .foregroundStyle(.originalYellow)
+                .stroke(color: .originalGreen, width: 5)
+            Spacer()
+            Button {
                 isShowingQuizView = true
+            } label: {
+                Image(.startButton)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
             }
             .fullScreenCover(isPresented: $isShowingQuizView) {
                 QuizView()
             }
         }
         .padding()
+        .backgroundImage()
     }
 }
 
