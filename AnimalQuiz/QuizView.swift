c42dd6fc081dd7aceb1ab6c7d2071af3397543aa
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-02 16:09:44 +0900

    問題を追加

--- AnimalQuiz/QuizView.swift
+++ AnimalQuiz/QuizView.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
+struct QuizItem {
+    let question: String
+    var choices: [String]
+    let correctAnswer: String
+}
+
 struct QuizView: View {
     @State var isShowingScoreView = false
     let choices = ["ライオン","ウサイン・ボルト","チーター","馬"]
+    
+    let quizItems = [
+        QuizItem(
+            question: "次のうち、世界で最も早く走る動物はどれですか？",
+            choices: ["ライオン","ウサイン・ボルト","チーター","ウマ"],
+            correctAnswer: "チーター"
+        ),
+        QuizItem(
+            question: "次のうち、飛ぶことができない鳥はどれですか？",
+            choices: ["ペンギン","フクロウ","ハト","スズメ"],
+            correctAnswer: "ペンギン"
+        ),
+        QuizItem(
+            question: "次のうち、哺乳類でない動物はどれですか？",
+            choices: ["イルカ","カメ","コウモリ","ヒト"],
+            correctAnswer: "カメ"
+        ),
+        QuizItem(
+            question: "次のうち、夜行性でない動物はどれですか？",
+            choices: ["ライオン","コアラ","ゾウ","フクロウ"],
+            correctAnswer: "ゾウ"
+        ),
+        QuizItem(
+            question: "次のうち、最も長い首を持つ動物はどれですか？",
+            choices: ["キリン","アルパカ","ゾウ","ウマ"],
+            correctAnswer: "キリン"
+        )
+    ]
     var body: some View {
         VStack {
             Text("問題番号: 1/5")
@@ -18,7 +52,7 @@ struct QuizView: View {
                 .background(Color.originalGreen)
                 .foregroundStyle(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
-            Text("次のうち、世界で最も早く走る動物はどれですか？")
+            Text(quizItems[0].question)
                 .font(.title)
                 .padding()
                 .frame(maxWidth: .infinity)
@@ -28,7 +62,7 @@ struct QuizView: View {
                         .stroke(.originalGreen, lineWidth: 5)
                 )
                 .frame(maxHeight: .infinity)
-            ForEach(choices, id: \.self) { choice in
+            ForEach(quizItems[0].choices, id: \.self) { choice in
                 Button {
                     isShowingScoreView = true
                 } label: {
