fdc3dc7d0764bf122898bdfd7af5c3a47ab5a265
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-02 16:24:15 +0900

    カンマの後ろにスペースを追加した

--- AnimalQuiz/QuizView.swift
+++ AnimalQuiz/QuizView.swift
@@ -15,32 +15,31 @@ struct QuizItem {
 
 struct QuizView: View {
     @State var isShowingScoreView = false
-    let choices = ["ライオン","ウサイン・ボルト","チーター","馬"]
-    
+    let choices = ["ライオン", "ウサイン・ボルト", "チーター", "馬"]
     let quizItems = [
         QuizItem(
             question: "次のうち、世界で最も早く走る動物はどれですか？",
-            choices: ["ライオン","ウサイン・ボルト","チーター","ウマ"],
+            choices: ["ライオン", "ウサイン・ボルト", "チーター", "ウマ"],
             correctAnswer: "チーター"
         ),
         QuizItem(
             question: "次のうち、飛ぶことができない鳥はどれですか？",
-            choices: ["ペンギン","フクロウ","ハト","スズメ"],
+            choices: ["ペンギン", "フクロウ", "ハト", "スズメ"],
             correctAnswer: "ペンギン"
         ),
         QuizItem(
             question: "次のうち、哺乳類でない動物はどれですか？",
-            choices: ["イルカ","カメ","コウモリ","ヒト"],
+            choices: ["イルカ", "カメ", "コウモリ", "ヒト"],
             correctAnswer: "カメ"
         ),
         QuizItem(
             question: "次のうち、夜行性でない動物はどれですか？",
-            choices: ["ライオン","コアラ","ゾウ","フクロウ"],
+            choices: ["ライオン", "コアラ", "ゾウ", "フクロウ"],
             correctAnswer: "ゾウ"
         ),
         QuizItem(
             question: "次のうち、最も長い首を持つ動物はどれですか？",
-            choices: ["キリン","アルパカ","ゾウ","ウマ"],
+            choices: ["キリン", "アルパカ", "ゾウ", "ウマ"],
             correctAnswer: "キリン"
         )
     ]
