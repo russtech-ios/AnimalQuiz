7be67fb1ab3a2296aa4ba31eca09234be4fc5d5c
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-03 16:17:56 +0900

    警告を全て消した

--- AnimalQuiz/View+.swift
+++ AnimalQuiz/View+.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 extension View {
-    @ViewBuilder
     func stroke(color: Color, width: CGFloat = 1) -> some View {
         modifier(StrokeBackground(strokeSize: width, strokeColor: color))
     }
+
     @ViewBuilder
     func backgroundImage(_ image: Image = Image(.background)) -> some View {
-        self
-            .background(
-                image
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
-                    .padding(-3)
-            )
+        background(
+            image
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .padding(-3)
+        )
     }
 }
