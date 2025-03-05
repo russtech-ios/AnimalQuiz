e2d40bbbfa91e79fe6c8ebb4f54265c35f14ae0a
rasu-ios <n.russ.m.appdeveloper@gmail.com>
2025-03-05 13:50:44 +0900

    クイズデータを別のファイルに移動させた。

--- AnimalQuiz/Views/QuizView.swift
+++ AnimalQuiz/Views/QuizView.swift
@@ -19,33 +19,7 @@ struct QuizView: View {
     @State var isAnswerCorrect = false
     @State var currentQuizIndex = 0
     @State var correctCount = 0
-    let quizItems = [
-        QuizItem(
-            question: "次のうち、世界で最も早く走る動物はどれですか？",
-            choices: ["ライオン", "ウサイン・ボルト", "チーター", "ウマ"],
-            correctAnswer: "チーター"
-        ),
-        QuizItem(
-            question: "次のうち、飛ぶことができない鳥はどれですか？",
-            choices: ["ペンギン", "フクロウ", "ハト", "スズメ"],
-            correctAnswer: "ペンギン"
-        ),
-        QuizItem(
-            question: "次のうち、哺乳類でない動物はどれですか？",
-            choices: ["イルカ", "カメ", "コウモリ", "ヒト"],
-            correctAnswer: "カメ"
-        ),
-        QuizItem(
-            question: "次のうち、夜行性でない動物はどれですか？",
-            choices: ["ライオン", "コアラ", "ゾウ", "フクロウ"],
-            correctAnswer: "ゾウ"
-        ),
-        QuizItem(
-            question: "次のうち、最も長い首を持つ動物はどれですか？",
-            choices: ["キリン", "アルパカ", "ゾウ", "ウマ"],
-            correctAnswer: "キリン"
-        )
-    ]
+    let quizItems = QuizData.quizItems
     var body: some View {
         ZStack {
             VStack {
